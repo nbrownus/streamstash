3b61995ddb1c20a6cab4197c40fe0d393a46e0d7
Nate Brown <nbrown.us@gmail.com>
2014-01-03 14:44:43 -0800

    Adding docs, polishing event life cycle, exanping on the basic example

--- examples/basic.js
+++ examples/basic.js
@@ -4,5 +4,21 @@ addInputPlugin('stdin')
 addOutputPlugin('stdout')
 
 addFilter(function (event) {
-    event.done()
+    event.data.filter1 = true
+
+    if (event.data.message == 'done') {
+        return event.done()
+    }
+
+    event.next()
+})
+
+addFilter(function (event) {
+    event.data.filter2 = true
+
+    if (event.data.message == 'cancel') {
+        return event.cancel()
+    }
+
+    event.next()
 })
