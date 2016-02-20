39c6d0aed063f7d4ca4de9118a394ac95a6623e1
Nate Brown <nbrown.us@gmail.com>
2016-02-19 17:59:04 -0800

    Fixing docker example doc

--- examples/relp_container.js
+++ examples/relp_container.js
@@ -1,20 +1,5 @@
 /**
- * Takes input from rsyslog via the omrelp output plugin and outputs event objects to stdout while in a container
- *
- * Try it by running
- *
- *      ./bin/streamstash examples/relp_container.js
- *
- * Configure your rsyslog instance to talk us and restart
- *
- *      module(load="omrelp")
- *      action(type="omrelp" name="streamstash_relp" target="IP_FOR_CONTAINER_HOST" port="5514")
- *
- * Make sure you have rsyslog-relp installed (ubuntu/debian)
- *
- * Send some log lines to rsyslog
- *
- *      logger "HI THERE"
+ * Just like `relp_basic.js` but this one is for the docker example
  */
 
 addInputPlugin('relp', { host: '0.0.0.0', port: 5514, codec: null })
