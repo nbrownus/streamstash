a9247bbfbb0f7c69a268e781fd33182ef2156d78
Nate Brown <nbrown.us@gmail.com>
2016-03-01 21:40:12 -0800

    Finish removing codecs

--- examples/relp_container.js
+++ examples/relp_container.js
@@ -2,5 +2,5 @@
  * Just like `relp_basic.js` but this one is for the docker example
  */
 
-addInputPlugin('relp', { host: '0.0.0.0', port: 5514, codec: null })
+addInputPlugin('relp', { host: '0.0.0.0', port: 5514 })
 addOutputPlugin('stdout')
