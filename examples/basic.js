69633f85483c3cb76a73306f2656d878bb6f4614
Nate Brown <nbrown.us@gmail.com>
2016-03-01 21:29:13 -0800

    Update examples for new parsers

--- examples/basic.js
+++ examples/basic.js
@@ -7,7 +7,6 @@
  *
  * Type a message and hit enter
  */
-var util = require('util')
 
 //Pause all inputs if the number of in process events exceeds the highWatermark
 streamStash.highWaterMark = 1000
@@ -20,16 +19,14 @@ addInputPlugin('stdin')
 addOutputPlugin('stdout')
 
 addFilter(function (event) {
-    var data
-
-    try {
-        data = JSON.parse(event.data.message)
-        event.data = util._extend(event.data, data)
-        event.data.message = event.data['@message']
-        delete event.data.originalMessage
-
-    } catch (error) {
-        event.data['@type'] = 'unparsable'
+    // parsers is a global object containing all parsers built into StreamStash
+    // If the parser fails event.data will contain `@type` set to 'unparseable' and `parseError` will be
+    // the reason the parser failed
+    // If the parser succeeds the original message will be backed up in event.data.originalMessage
+    // The third argument controls whether or not the original message is backed up,
+    // set it to false or don't provide it to disable the feature
+    if (!parsers.jsonParser(event, '@type', true)) {
+        return event.done()
     }
 
     event.data.filter1 = true
